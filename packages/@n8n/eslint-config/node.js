9155cf8d139789abb5e1fc821462e3980cde9f07
Pedro-phc <negociosonline.phc@gmail.com>
2025-04-15 21:24:26 -0300

    Update node.js

--- packages/@n8n/eslint-config/node.js
+++ packages/@n8n/eslint-config/node.js
@@ -1,11 +1,17 @@
-# Descarregar e instalar a fnm:
-winget install Schniz.fnm
+# Docker possui instruções de instalação específicas para cada sistema operativo.
+# Podemos consultar a documentação oficial no https://docker.com/get-started/
 
-# Descarregar e instalar a Node.js:
-fnm install 18
+# Puxar ou extrair a imagem de Docker da Node.js:
+docker pull node:18-alpine
+
+# Criar um contentor de Node.js e iniciar uma sessão de Shell:
+docker run -it --rm --entrypoint sh node:18-alpine
 
 # Consultar a versão da Node.js:
 node -v # Deveria imprimir "v18.20.8".
 
-# Consultar a versão da npm:
-npm -v # Deveria imprimir "10.8.2".
+# Descarregar e instalar a pnpm:
+corepack enable pnpm
+
+# Consultar a versão da pnpm:
+pnpm -v
